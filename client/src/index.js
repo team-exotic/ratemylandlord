eed17e63f9a9c32f8d0aa280eac33f5867d6d2c9
lancej1022 <lancej1022@gmail.com>
2020-04-14 00:19:24 -0700

    finished basic hero section

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
+import './owl.theme.default.scss';
+import './owl.carousel.scss';
 import './main_styles.scss';
 import './responsive.scss';
 import Home from './views/Home/Home';
+import Login from './views/Login/Login';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Home />
+      <Switch>
+        <Route path="/" exact render={(props) => <Home {...props} />} />
+        <Route path="/" exact render={(props) => <Login {...props} />} />
+      </Switch>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
