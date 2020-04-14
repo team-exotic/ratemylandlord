00334e6ebd3ac7c10e908ff9e034f8cb3ef371de
Aris Razuri <arazuli4@gmail.com>
2020-04-13 21:14:01 -0700

    added boilerplate for routes

---
+++ server/routes/api.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const apiRouter = express.Router();
+
+// add middleware (controllers)
+
+// api routes
+
+module.exports = apiRouter;
--- server/server.js
+++ server/server.js
@@ -9,10 +9,32 @@ const PORT = process.env.port || 8000;
 // parse JSON and cookies
 app.use(express.json());
 app.use(cookieParser());
+
 //serve static files
-app.use('/public', express.static(path.join(__dirname, '../public')));
+app.use('/public', express.static(path.join(__dirname, '../client/public')));
+
+//serve index.html
+app.get('/', (req, res) =>
+  res.status(200).sendFile(path.resolve(__dirname, '../client/public/index.html'))
+);
 
 //Require in routes
-const loginRouter
+const loginRouter = require('./routes/login');
+const apiRouter = require('./routes/api');
+
+//route handlers
+app.use('/login', loginRouter);
+app.use('/api', apiRouter);
+
+//catch-all route handler for any unknown route requests
+app.use((req, res) => res.sendStatus(404));
+
+//global error handler
+app.use((err, req, res, next) => {
+  console.log(`global error handler caught unknown middleware error: ${err}`);
+  res.status(400).json({ err: 'an error has occurred' });
+});
 
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
+
+module.exports = app;
