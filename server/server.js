22b2655de2c45d91c12103fdd0a77352ed72da65
Aris Razuri <arazuli4@gmail.com>
2020-04-13 19:25:53 -0700

    added to server.js

--- server/server.js
+++ server/server.js
@@ -9,5 +9,10 @@ const PORT = process.env.port || 8000;
 // parse JSON and cookies
 app.use(express.json());
 app.use(cookieParser());
+//serve static files
+app.use('/public', express.static(path.join(__dirname, '../public')));
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+//Require in routes
+const loginRouter
+
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
