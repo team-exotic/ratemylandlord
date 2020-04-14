ddf0b014a959f74991ed88d6b13f85fcc34c0c40
Aris Razuri <arazuli4@gmail.com>
2020-04-14 14:28:52 -0700

    add boilerplate for userController

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,7 +3,30 @@ const express = require('express');
 const loginRouter = express.Router();
 
 // add middleware (controllers)
+//or signup
+loginRouter.post(
+  '/login',
+  userController.verifyUser,
+  cookieController.setCookie, //setcookie and store token in user table w/expiration?
+  (req, res) => {
+    if (res.locals.err) {
+      res.redirect('/login', { error: res.locals.err });
+    }
+    res.redirect('/'); // last page they were in
+  }
+);
 
+loginRouter.post(
+  '/signup',
+  userController.createUser,
+  cookieController.setCookie,
+  (req, res) => {
+    if (res.locals.err) {
+      res.redirect('/signup', { error: res.locals.err });
+    }
+    res.redirect('/'); // last page they were in
+  }
+);
 // api routes
 
 module.exports = loginRouter;
