adf70125764e5403cb601777bc4dc805c57c3117
Aris Razuri <arazuli4@gmail.com>
2020-04-15 14:07:09 -0700

    finished basic func of propertyProfile controller

--- server/controllers/propertyController.js
+++ server/controllers/propertyController.js
@@ -37,24 +37,25 @@ propertyController.addComment = (req, res, next) => {};
 //gives list of a matching addresses
 propertyController.searchByAddress = (req, res, next) => {
   const { address, name } = req.body;
+  let propertyQuery;
   if (name !== '') {
-    const propertyQuery = {
+    propertyQuery = {
       text: 'SELECT * FROM "property" WHERE name=$1 or address=$2 ',
       values: [name, address]
     };
   } else {
-    const propertyQuery = {
+    propertyQuery = {
       text: 'SELECT * FROM "property" WHERE address=$1',
       values: [address]
     };
   }
   db.query(propertyQuery)
     .then((property) => {
-      console.log('in db query for searchbyaddress data received:', property);
+      //console.log('in db query for searchbyaddress data received:', property);
       if (property.rows.length === 0) {
         return (res.locals.matchedFound = false);
       } else {
-        res.locals.property = property.rows;
+        res.locals.property = property.rows; // [{id:'',name: '', address: '', image: ''},etc.]
       }
       return next();
     })
@@ -68,6 +69,56 @@ propertyController.searchByAddress = (req, res, next) => {
 propertyController.searchByCity = (req, res, next) => {};
 
 //find the profile page (property row w/all comments and sections)
-propertyController.propertyProfile = (req, res, next) => {};
+propertyController.propertyProfile = (req, res, next) => {
+  const { id } = req.body;
+  //console.log('inside controller of profile, id is:', id);
+  const profileQuery = {
+    text: `WITH property_ratings as (
+      SELECT 
+       property_id,
+       AVG(timely_maintenance) as tm,
+       AVG(appropriate_distance) as dist,
+       AVG(respectful) as res,
+       AVG(communication) as comm,
+       AVG(flexibility) as flex,
+       AVG(transparency) as tran,
+       AVG(organized) as org,
+       AVG(professionalism) as prof
+      FROM "rating"
+      GROUP BY property_id
+      ),
+      property_comments as (
+      SELECT 
+       property_id,
+       comment,
+       created_at
+      FROM comments
+      )
+      SELECT 
+        p.*,
+        pr.*,
+        pc.*
+      FROM
+       "property" p
+       LEFT OUTER JOIN property_ratings pr
+        on p.id = pr.property_id
+       LEFT OUTER JOIN property_comments pc
+        on p.id = pc.property_id
+      WHERE
+       p.id = $1 ORDER BY pc.created_at ASC `,
+    values: [id]
+  };
+  db.query(profileQuery)
+    .then((profile) => {
+      console.log('this is returned sql res for profile', profile.rows);
+      res.locals.propertyProfile = profile.rows;
+      return next();
+    })
+    .catch((err) => {
+      next({
+        log: `error in middleware propertyController.propertyProfile: ${err}`
+      });
+    });
+};
 
 module.exports = propertyController;
--- server/routes/api.js
+++ server/routes/api.js
@@ -10,7 +10,20 @@ apiRouter.post('/property', (req, res) => {});
 
 //search property by address
 apiRouter.get('/property', propertyController.searchByAddress, (req, res) => {
-  res.status(200).json(res.locals);
+  if (res.locals.matchedFound === false) {
+    res.json({
+      error: 'no matches found, please try again'
+    });
+  } else if (res.locals.err) {
+    res.json({ error: res.locals.err });
+  } else {
+    res.status(200).json(res.locals.property);
+  }
+});
+
+//get property profile page by propertyID
+apiRouter.get('/property/:id', propertyController.propertyProfile, (req, res) => {
+  res.status(200).json(res.locals.propertyProfile);
 });
 
 //search property by city
