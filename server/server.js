efca999a8148b0d302e177e1242bbbcd6aebc602
MrAustin2u <aaustin86@gmail.com>
2020-04-14 20:12:53 -0700

    Created createUser middleware in userController.js. Testing functionality in PostMan, returns request body. Also tested that createUser middleware writes to database in user table.

--- server/server.js
+++ server/server.js
@@ -3,34 +3,38 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.port || 8000;
-// parse JSON and cookies
-app.use(express.json());
+// parse JSON and return req.bodycookies
+app.use(bodyParser.json());
+/*automatically parse urlencoded body content from incoming
+request and place it in req.body*/
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-//serve static files
+// serve static files
 app.use('/public', express.static(path.join(__dirname, '../client/public')));
 
-//serve index.html
+// serve index.html
 app.get('/', (req, res) =>
   res.status(200).sendFile(path.resolve(__dirname, '../client/public/index.html'))
 );
 
-//Require in routes
+// Require in routes
 const loginRouter = require('./routes/login');
 const apiRouter = require('./routes/api');
 
-//route handlers
-app.use('/login', loginRouter);
+// route handlers
+app.use('/account', loginRouter);
 app.use('/api', apiRouter);
 
-//catch-all route handler for any unknown route requests
+// catch-all route handler for any unknown route requests
 app.use((req, res) => res.sendStatus(404));
 
-//global error handler
-app.use((err, req, res, next) => {
+// global error handler
+app.use((err, req, res) => {
   console.log(`global error handler caught unknown middleware error: ${err}`);
   res.status(400).json({ err: 'an error has occurred' });
 });
