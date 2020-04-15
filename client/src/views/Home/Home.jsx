46bb00f14bf471e234a099bca322cc8204d07244
MrAustin2u <aaustin86@gmail.com>
2020-04-14 20:50:43 -0700

    Client folder fixed

---
+++ client/src/views/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from '../../components/HeroSection/Hero';
+
+const Home = () => {
+  return (
+    <div className="super_container">
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
