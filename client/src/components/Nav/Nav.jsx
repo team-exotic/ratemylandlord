2baf96755971e51b2d81138709e3e9984df4b1b3
lancej1022 <lancej1022@gmail.com>
2020-04-15 15:42:27 -0700

    minor navbar changes

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 // build navr bar here and import it into hom.jsx
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faBuilding } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
@@ -9,32 +9,32 @@ const NavBar = () => {
   const [isLoggedIn, setLogin] = useState(false);
 
   return (
-    <header class="header">
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <div class="header_content d-flex flex-row align-items-center justify-content-start">
-              <div class="logo">
-                <a href="#">
+    <header className="header">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="header_content d-flex flex-row align-items-center justify-content-start">
+              <div className="logo">
+                <Link to="/">
                   {/* <img src="images/logo.png" alt></img> */}
                   <div className="nav_title">
                     <FontAwesomeIcon color="white" icon={faBuilding} />
                     <span> Rate My Landlord </span>
                   </div>
-                </a>
+                </Link>
               </div>
-              <nav class="phone_num ml-auto">
-                <Link className="phone_num_inner" Link to="/">
+              <nav className="phone_num ml-auto">
+                <Link className="phone_num_inner" to="/">
                   <span> Home </span>{' '}
                 </Link>
-                <Link className="phone_num_inner" Link to="/login">
+                <Link className="phone_num_inner" to="/login">
                   <span> Login </span>{' '}
                 </Link>
-                <Link className="phone_num_inner" Link to="/signup">
+                <Link className="phone_num_inner" to="/signup">
                   <span> Signup </span>
                 </Link>
               </nav>
-              <div class="hamburger ml-auto">
+              <div className="hamburger ml-auto">
                 <FontAwesomeIcon color="white" icon={faBars} />
               </div>
             </div>
