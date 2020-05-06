878902e715d13ff22d7b5e9a9fb854bd7fd8c6e1
lancej1022 <lancej1022@gmail.com>
2020-05-05 19:57:30 -0700

    Fixed responsive css issues for mobile devices.
    The home page of the application had some "buggy" css issues on mobile displays, causing some text not to render properly as well as other text running into each other. To fix this, certain text was hidden on mobile devices and line-height were fixed elsewhere.
    
    While the homepage could be polished some more, this commit helps make it at least functionally presentable without glaring issues.
    
    Signed-off-by: Lance J lancej1022@gmail.com

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,20 +1,20 @@
 // build navr bar here and import it into hom.jsx
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { userActions } from '../../actions/userActions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faBuilding } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import { userActions } from '../../actions/userActions';
 import './Nav.scss';
 
 const NavBar = () => {
-  //redux store is connected here
+  // redux store is connected here
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.isLoggedIn);
 
-  //constantly checks to see if ther user is loggedin
+  // constantly checks to see if ther user is loggedin
   useEffect(() => {
-    //if the user is logged in dispatch login action.
+    // if the user is logged in dispatch login action.
     if (
       document.cookie.split(';').filter((item) => {
         return item.includes('isLoggedIn=');
@@ -27,7 +27,7 @@ const NavBar = () => {
     // }
   });
 
-  //dispatches logout action if the user wants to logout
+  // dispatches logout action if the user wants to logout
   const handleClick = () => {
     dispatch(userActions.logout());
   };
@@ -61,12 +61,12 @@ const NavBar = () => {
                   </Link>
                 )}
                 <Link className="phone_num_inner" to="/signup">
-                  {user ? <span></span> : <span> Signup </span>}
+                  {user ? <span /> : <span> Signup </span>}
                 </Link>
               </nav>
-              <div className="hamburger ml-auto">
+              {/* <div className="hamburger ml-auto">
                 <FontAwesomeIcon color="white" icon={faBars} />
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
